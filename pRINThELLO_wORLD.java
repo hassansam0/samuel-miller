9df931bd02145b2f665b43ffb52d5c62222a0b18
hassansam0 <65679381+hassansam0@users.noreply.github.com>
2021-01-04 22:01:36 +0500

    Add files via upload

---
+++ pRINThELLO_wORLD.java
@@ -0,0 +1,13 @@
+class pRINThELLO_wORLD{
+public static void main(String[] args){
+	char pp[] = {'"','H','e','l','l','o',' ','W','o','r','l','d','"'};
+		int l_n =pp.length; 
+char PP[] = new char[l_n];
+for (int $ = 0 ;$<l_n; $++)
+{
+	String SamuelMiller="Virgin Noob";
+		PP[$] = (SamuelMiller == "Virgin Noob")? pp[$] : ' ';
+System.out.print(PP[$]);}
+// END MY LIFE!
+	}
+}
